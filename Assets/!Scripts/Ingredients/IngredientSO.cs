e698a317dc0824f1a2fc30d5ebd2cb8cb1b83276
James Mitchell <v010000010101001101010011@gmail.com>
2025-05-16 02:23:36 -0400

    added magic circle symbols

--- Assets/!Scripts/Ingredients/IngredientSO.cs
+++ Assets/!Scripts/Ingredients/IngredientSO.cs
@@ -8,18 +8,4 @@ public class IngredientSO : ItemData
     [Space]
     [Range(0.5f, 5.0f)] public float CostMultiplier = 1.0f;
     public PotionEffect PotionEffect;
-    public ulong DescriptorFlags = 0;
-    [TextArea]
-    public string DeserializedDescriptors = string.Empty;
-
-    [ContextMenu("Serialize Descriptors")]
-    public void SerializeDescriptors()
-    {
-
-    }
-
-    private static Dictionary<string, byte> _descriptorFlagLookup = new Dictionary<string, byte>()
-    {
-        
-    };
 }
