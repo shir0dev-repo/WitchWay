159e2cb18403fe2dddc44a54de3b94686ec508f6
StarlionGames <starliongames.dev@gmail.com>
2025-05-31 13:30:03 -0400

    Adjusted levels and added toggles
    - filling the bottle all the way turns the siphon off
    - added bool for bottle

--- Assets/!Scripts/Crafting/Stations/Bottling/BottleLevel.cs
+++ Assets/!Scripts/Crafting/Stations/Bottling/BottleLevel.cs
@@ -8,41 +8,53 @@ public class BottleLevel : MonoBehaviour
 
     public float amount = 0f;
     float rateOfStream = 0f;
+    bool bottleIsFull = false;
 
     void Update()
     {
-        if (CheckIfFilled())
+        if (CheckIfFilled() || bottleIsFull)
         {
             return;
+            // checks if the bottle is filled either via the function or the bool
+            // i'll change this so there's an event... somewhere
         }
-        else
-        {
-            RaiseBottleLevel();
-            amount += Time.deltaTime * rateOfStream;
-            Mathf.Clamp(amount, 0f, 100);
-        }
+        RaiseStreamLevel();
+        FillBottle();
         
         slider.SetValue(amount);
     }
-    void RaiseBottleLevel()
+    void RaiseStreamLevel()
     {
         float i = Siphon.instance.GetCurrentPressureAmount();
 
-        if (i < 25) { rateOfStream = 1; }
-        else if (i > 50) { rateOfStream = 5; }
-        else if (i > 75) { rateOfStream = 15; }
+        if (i == 0) { rateOfStream = 0; }
+        else if (i <= 25) { rateOfStream = 1; }
+        else if (i <= 50) { rateOfStream = 5; }
+        else if (i <= 80) { rateOfStream = 10; }
+        else { rateOfStream = 15; }
+        // if there's a better way to do this please replace this
+    }
+    void FillBottle()
+    {
+        if (rateOfStream == 0)
+        {
+            return;
+            // if the player hasn't pressed the button, the bottle won't fill up
+        }
+        amount += Time.smoothDeltaTime * rateOfStream;
+       
+        Mathf.Clamp(amount, 0, 100);
     }
     bool CheckIfFilled()
     {
-        if (amount >= 100)
+        if (amount >= 100 && !bottleIsFull)
         {
+            bottleIsFull = true;
             Debug.Log("The bottle is full!");
+            Siphon.instance.ToggleButtonAbility();
             return true;
             // later, add stuff to disable the pump if the bottle is full
         }
-        else
-        {
-            return false;
-        }
+        return false;
     }
 }
--- Assets/!Scripts/Crafting/Stations/Bottling/Siphon.cs
+++ Assets/!Scripts/Crafting/Stations/Bottling/Siphon.cs
@@ -4,9 +4,10 @@ public class Siphon : MonoBehaviour
 {
     public static Siphon instance {  get; private set; }
     [SerializeField] SliderBar slider;
+    // siphon requires it's own slider
 
     public float pressureAmount = 0;
-
+    bool canPressButton = true;
     void Start()
     {
         if (instance != null && instance != this)
@@ -21,12 +22,15 @@ public class Siphon : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canPressButton)
         {
             IncreasePressure();
         }
+        if (canPressButton)
+        {
+            DecreasePressure();
+        }
 
-        DecreasePressure();
         slider.SetValue(pressureAmount);
     }
 
@@ -44,7 +48,6 @@ public class Siphon : MonoBehaviour
 
         pressureAmount = ClampPressureAmount();
     }
-
     float ClampPressureAmount()
     {
         return Mathf.Clamp( pressureAmount, 0, 100);
@@ -52,5 +55,16 @@ public class Siphon : MonoBehaviour
     public float GetCurrentPressureAmount()
     {
         return pressureAmount;
+    }
+    public void ToggleButtonAbility()
+    {
+        canPressButton = !canPressButton;
+        ResetPressure();
+        // whenever the ability to increase the pressure is toggled, reset the bar's progress
+    }
+    public void ResetPressure()
+    {
+        pressureAmount = 0;
+        slider.SetValue(pressureAmount);
     }
 }
