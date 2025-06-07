6fbf190b058bb91c5e449d5e320a2a17b4c3dc99
StarlionGames <starliongames.dev@gmail.com>
2025-06-06 20:22:49 -0400

    added a lot but still jank
    hold on

--- Assets/!Scripts/Crafting/Stations/BoilingCooling/PotTemperature.cs
+++ Assets/!Scripts/Crafting/Stations/BoilingCooling/PotTemperature.cs
@@ -20,24 +20,46 @@ public class PotTemperature : MonoBehaviour
 
     void Update()
     {
+        EqualOutTemp();
         slider.SetValue(Temperature);
     }
     public void RaiseTemp(float amount)
     {
         Temperature += amount;
-        ClampTemp();
     }
     public void LowerTemp(float amount)
     {
         Temperature -= amount;
-        ClampTemp();
     }
     public float GetCurrentTemp()
     {
         return Temperature;
     }
+    void EqualOutTemp()
+    {
+        float toMiddle = Mathf.Clamp01(Mathf.Abs(Temperature) * 0.01f);
+        float value = Mathf.Lerp(50, 0, toMiddle);
+        // toMiddle is the temp's percentage away from zero, no matter if its pos or neg
+        // value is the easing towards zero
+        
+        if (Temperature > 0)
+        {
+            Temperature -= value * Time.deltaTime;
+            if (Temperature < 0.001f) Temperature = 0;
+            // snapping to zero to prevent infinite calculations
+        }
+        else if (Temperature < 0)
+        {
+            Temperature += value * Time.deltaTime;
+            if (Temperature > 0.001f) Temperature = 0;
+        }
+        else { Temperature = 0; }
+        
+        ClampTemp();
+    }
     void ClampTemp()
     {
-        Temperature = Mathf.Clamp(Temperature, -50, 50);
+        if (Temperature > 0) { Temperature = Mathf.Clamp(Temperature, 0, 50); }
+        else { Temperature = Mathf.Clamp(Temperature, -50, 0); }
     }
 }
--- Assets/!Scripts/Crafting/Stations/BoilingCooling/TemperatureButtons.cs
+++ Assets/!Scripts/Crafting/Stations/BoilingCooling/TemperatureButtons.cs
@@ -5,11 +5,30 @@ public class TemperatureButtons : MonoBehaviour
     public bool isHeating;
 
     [SerializeField]
-    float value = 1f;
+    float baseValue = 5f;
 
     private void OnMouseOver()
     {
-        if (isHeating) { PotTemperature.Instance.RaiseTemp(value); }
-        else {  PotTemperature.Instance.LowerTemp(value); }
+        float currTemp = PotTemperature.Instance.GetCurrentTemp();
+
+        float toMiddle = Mathf.Clamp01(Mathf.Abs(currTemp)/50);
+        float ease = Easing(toMiddle);
+        // toMiddle is the current temp away from zero
+
+        float value = baseValue * ease * Time.smoothDeltaTime;
+        
+        if (isHeating)
+        {
+            PotTemperature.Instance.RaiseTemp(value);
+        }
+        else
+        {
+            PotTemperature.Instance.LowerTemp(value);
+        }
+        Debug.Log($"Temperature: {currTemp} \ntoMiddle: {toMiddle}\neasing: {ease}n\value: {value}");
+    }
+    float Easing(float x)
+    {
+        return x * x * x * x * x;
     }
 }
