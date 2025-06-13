404e2a11d11a676c3f04ce7a62356c49b67bcedb
MrDerpFace610110 <bman610110@gmail.com>
2025-06-13 19:44:25 -0400

    added jumping and crouch

--- Assets/!Scripts/WitchingZone/WZPlayerController.cs
+++ Assets/!Scripts/WitchingZone/WZPlayerController.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/*
+Possible improvements:
+    - jump buffering
+*/
+
 //this script is big may be good to split it
 public class WZPlayerController : MonoBehaviour
 {
@@ -29,27 +34,47 @@ public class WZPlayerController : MonoBehaviour
     [Space(5)]
     [SerializeField] private InputAction pauseAction;
 
-    [Header("Tweaking")] //prob needs a better name
+    [Header("Move and Look values")]
     [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private float lookSpeed = 2.0f;
+
+    [Header("Jump Vars")]
+    [SerializeField] private float jumpPower = 10;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private float groundDist = 0.4f;
+
+    [Header("Crouch Vars")]
+    [SerializeField] private float crouchSpeed = 2.5f;
+    [SerializeField] private float percentOrigHeight;
 
     //private vars
     //input
     private Vector2 moveInput = Vector2.zero;
     private Vector2 lookInput = Vector2.zero;
+    private float currentSpeed;
 
     //refs
     private Rigidbody rb;
     private Transform camTransform;
     private float cameraPitch = 0.0f;
 
+    private Vector3 originalScale;
+    private bool wasGrounded = false;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         camTransform = GetComponentInChildren<Camera>().transform;
 
+        originalScale = transform.localScale;
+
         //lock mouse
         Cursor.lockState = CursorLockMode.Locked;
+
+        currentSpeed = moveSpeed;
+
+        wasGrounded = Physics.CheckSphere(groundCheckPos.position, groundDist, groundLayer);
     }
 
     void OnEnable()
@@ -57,6 +82,8 @@ public class WZPlayerController : MonoBehaviour
         //enable input actions
         moveAction.Enable();
         lookAction.Enable();
+        jumpAction.Enable();
+        crouchAction.Enable();
 
         //subscibe to events
         moveAction.performed += SetMoveValues;
@@ -64,6 +91,11 @@ public class WZPlayerController : MonoBehaviour
 
         lookAction.performed += SetLookValues;
         lookAction.canceled += SetLookValues;
+
+        jumpAction.performed += DoJump;
+
+        crouchAction.performed += DoCrouch;
+        crouchAction.canceled += UnDoCrouch;
     }
 
     void OnDisable()
@@ -75,9 +107,16 @@ public class WZPlayerController : MonoBehaviour
         lookAction.performed -= SetLookValues;
         lookAction.canceled -= SetLookValues;
 
+        jumpAction.performed -= DoJump;
+
+        crouchAction.performed -= DoCrouch;
+        crouchAction.canceled -= UnDoCrouch;
+
         //disable
         moveAction.Disable();
         lookAction.Disable();
+        jumpAction.Disable();
+        crouchAction.Disable();
     }
 
     void Update()
@@ -88,6 +127,14 @@ public class WZPlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         DoMove();
+
+        //grounded check
+         bool isGrounded = Physics.CheckSphere(groundCheckPos.position, groundDist, groundLayer);
+        if (!wasGrounded && isGrounded)
+        {
+            if (crouchAction.inProgress) ApplyCrouch();
+        }
+        wasGrounded = isGrounded;
     }
 
     //event for WASD control to update move value
@@ -108,7 +155,7 @@ public class WZPlayerController : MonoBehaviour
         if (moveInput.sqrMagnitude > 0.01f)
         {
             Vector3 move = transform.forward * moveInput.y + transform.right * moveInput.x;
-            rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + move * currentSpeed * Time.fixedDeltaTime);
         }
     }
 
@@ -124,4 +171,55 @@ public class WZPlayerController : MonoBehaviour
 
         camTransform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
     }
+
+    private void DoJump(InputAction.CallbackContext context)
+    {
+        if (Physics.CheckSphere(groundCheckPos.position, groundDist, groundLayer))
+        {
+            transform.localScale = originalScale;
+
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+            rb.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    //crouch button held
+    private void DoCrouch(InputAction.CallbackContext context)
+    {
+        if (Physics.CheckSphere(groundCheckPos.position, groundDist, groundLayer))
+        {
+            ApplyCrouch();
+        }
+    }
+
+    //crouch button released
+    private void UnDoCrouch(InputAction.CallbackContext context)
+    {
+        currentSpeed = moveSpeed;
+
+        transform.localScale = originalScale;
+    }
+
+    //should be changed many to anim in future or really just a camera offset
+    private void ApplyCrouch()
+    {
+        currentSpeed = crouchSpeed;
+
+        float scalePercent = percentOrigHeight / 100f;
+
+        Vector3 newScale = new Vector3(originalScale.x, originalScale.y * scalePercent, originalScale.z);
+        transform.localScale = newScale;
+
+        //set player position to the ground
+        transform.position = new Vector3(transform.position.x, transform.position.y - newScale.y, transform.position.z);
+    }
+
+    //debugging
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheckPos.position, groundDist); //visualize ground check
+    }
+#endif
 }
