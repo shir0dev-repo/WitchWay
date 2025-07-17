0336b7449b0a4416954298d3be41a38b7eecdb32
StarlionGames <starliongames.dev@gmail.com>
2025-07-17 14:13:11 -0400

    requested changes 1
    - replaced faulty test cauldron object with an amethyst prefab
    - added collider and z rotation clamping to cauldron's spoon

--- Assets/!Scripts/Crafting/Stations/Cauldron/CauldronSpoon.cs
+++ Assets/!Scripts/Crafting/Stations/Cauldron/CauldronSpoon.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class CauldronSpoon : ToolBase
 {
     CauldronMaster cauldron;
+    bool isSelected;
+    float maxZ = 300, minZ = 240;
 
     [SerializeField] Vector3 stirRotationEulers;
     [SerializeField] Vector3 restRotationEulers;
@@ -10,12 +12,23 @@ public class CauldronSpoon : ToolBase
     {
         cauldron = CauldronMaster.Instance;
     }
+    private void Update()
+    {
+        if (isSelected) { ClampZRotation(); }
+    }
+    void ClampZRotation()
+    {
+        float z = Mathf.Clamp(transform.rotation.eulerAngles.z, minZ, maxZ);
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, z);
+    }
     protected override void OnToolSelected()
     {
         gameObject.transform.rotation = Quaternion.Euler(stirRotationEulers);
+        isSelected = true;
     }
     protected override void OnToolDeselected()
     {
         gameObject.transform.rotation = Quaternion.Euler(restRotationEulers);
+        isSelected = false;
     }
 }
