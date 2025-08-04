bdfaa17f43e16e7ad5e526040aef5cb169a6451a
StarlionGames <starliongames.dev@gmail.com>
2025-08-04 12:59:34 -0400

    Switched around stuff
    - paintingCollection is now populated at start
    - paintings now have their own snappable object class that they can give to the puzzle room base
    - made some rudimentary behavior for rotating the paintings (still wip)
    - paintings have their positions randomized on start (but cannot be starting on their correct position)

--- Assets/!Scripts/Puzzles/Puzzleroom 4/PaintingSnapping.cs
+++ Assets/!Scripts/Puzzles/Puzzleroom 4/PaintingSnapping.cs
@@ -2,8 +2,24 @@ using UnityEngine;
 
 public class PaintingSnapping : MonoBehaviour
 {
-    bool _canSnap = false;
+    [SerializeField] SnappableObject _snappableObject;
 
+    bool _canSnap = false;
+    [SerializeField] Transform _correctPosition;
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    private void Update()
+    {
+        if (!_canSnap) { return; }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RotatePaintingToNextDirection();
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -17,5 +33,38 @@ public class PaintingSnapping : MonoBehaviour
         {
             _canSnap = false;
         }
+    }
+    void RotatePaintingToNextDirection()
+    {
+        Debug.Log("snapping to next cardinal position, not acutally tho");
+
+        if (transform == _correctPosition)
+        {
+            PuzzleRoomPainting o = gameObject.transform.GetComponentInParent<PuzzleRoomPainting>();
+            if (o != null) 
+            {
+                o.IsSolved();
+            }
+        }
+    }
+    void RandomizePosition()
+    {
+        Transform newTransform = _snappableObject.SnapPoints[Random.Range(0, 3)];
+
+        if (newTransform == _correctPosition)
+        {
+            newTransform = _snappableObject.SnapPoints[0];
+        }
+
+        gameObject.transform.position = newTransform.position;
+        gameObject.transform.rotation = newTransform.rotation;
+    }
+    public Transform GetCorrectPosition()
+    {
+        return _correctPosition;
+    }
+    public SnappableObject GetSnappableObject()
+    {
+        return _snappableObject;
     }
 }
--- Assets/!Scripts/Puzzles/Puzzleroom 4/PuzzleRoomPainting.cs
+++ Assets/!Scripts/Puzzles/Puzzleroom 4/PuzzleRoomPainting.cs
@@ -1,10 +1,33 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleRoomPainting : PuzzleBase
 {
-    [SerializeField] SnappableObject[] _paintingCollection;
+    SnappableObject[] _paintingCollection = new SnappableObject[0];
+    [SerializeField] List<GameObject> _paintings;
+
+    private void Start()
+    {
+        foreach (var painting in _paintings)
+        {
+            if (!painting.TryGetComponent(out PaintingSnapping paint)) { return; }
+            
+            Array.Resize(ref _paintingCollection, _paintingCollection.Length + 1);
+            _paintingCollection[_paintingCollection.Length - 1] = paint.GetSnappableObject();
+        }
+    }
+
     public override bool IsSolved()
     {
-        return false; // change later
+        foreach(SnappableObject painting in _paintingCollection)
+        {
+            if (painting.Object.gameObject)
+            {
+                return true; // placeholder for actual behaviour
+            }
+        }
+
+        return false;
     }
 }
