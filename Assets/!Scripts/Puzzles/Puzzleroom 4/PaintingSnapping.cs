1e1bf89049c6505c9dade1f336906eaa562036b9
StarlionGames <starliongames.dev@gmail.com>
2025-08-05 19:22:34 -0400

    Changing the puzzle
    - removed the unneeded code
    - removed the cardinal direction objects from the paintings
    - added the snapping points to the puzzle room prefab
    - paintings are now on the ground
    - when interacted with, the paintings tell you where they should be (placeholder)
    - no dragging yet

--- Assets/!Scripts/Puzzles/Puzzleroom 4/PaintingSnapping.cs
+++ Assets/!Scripts/Puzzles/Puzzleroom 4/PaintingSnapping.cs
@@ -2,88 +2,35 @@ using UnityEngine;
 
 public class PaintingSnapping : MonoBehaviour
 {
-    [SerializeField] SnappableObject _snappableObject;
+    [SerializeField] WZInteractable interact;
+    [SerializeField] string hint; // for debugging purposes
 
-    bool _canSnap = false;
+    bool _canPickUp = false;
     public bool _isInCorrectPosition = false;
-    int _currentIndex;
-    [SerializeField] Transform _correctPosition;
 
-    private void Start()
-    {
-        //RandomizePosition();
-        _currentIndex = 2;
-        ChangeGameObjectRotation(_currentIndex);
-    }
     private void Update()
     {
-        if (!_canSnap) { return; }
+        if (!_canPickUp) { return; }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            RotatePaintingToNextDirection();
+            interact.Interacted();
+            Debug.Log(hint);
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            _canSnap = true;
+            _canPickUp = true;
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            _canSnap = false;
+            _canPickUp = false;
         }
     }
-    void RotatePaintingToNextDirection()
-    { 
-        IterateThroughSnapPoints();
-        Debug.Log("snapping to next cardinal position: " + _snappableObject.SnapPoints[_currentIndex].name);
 
-        if (transform.localRotation == _correctPosition.localRotation)
-        {
-            Debug.Log("painting is in the right position!");
-            _isInCorrectPosition = true;
-
-            gameObject.GetComponentInParent<PuzzleRoomPainting>()?.IsSolved();
-            return;
-        }
-
-        _isInCorrectPosition = false;
-    }
-    void RandomizePosition()
-    {
-        _currentIndex = Random.Range(0, _snappableObject.SnapPoints.Length - 1);
-        ChangeGameObjectRotation(_currentIndex);
-    }
-    void IterateThroughSnapPoints()
-    {
-        _currentIndex++;
-
-        if (_currentIndex > _snappableObject.SnapPoints.Length - 1)
-        {
-            _currentIndex = 0;
-        }
-
-        ChangeGameObjectRotation (_currentIndex);
-    }
-    void ChangeGameObjectRotation(int nextTransform)
-    {
-        Transform newTransform = _snappableObject.SnapPoints[nextTransform];
-
-        gameObject.transform.rotation = newTransform.localRotation;
-        // the gameobject as a whole will rotate according to the local rotation
-        // that was set in the snap point's rotation (eg south = y180 (euler angles))
-    }
-    public Transform GetCorrectPosition()
-    {
-        return _correctPosition;
-    }
-    public SnappableObject GetSnappableObject()
-    {
-        return _snappableObject;
-    }
 }
--- Assets/!Scripts/Puzzles/Puzzleroom 4/PuzzleRoomPainting.cs
+++ Assets/!Scripts/Puzzles/Puzzleroom 4/PuzzleRoomPainting.cs
@@ -4,38 +4,10 @@ using UnityEngine;
 
 public class PuzzleRoomPainting : PuzzleBase
 {
-    SnappableObject[] _paintingCollection = new SnappableObject[0];
-    [SerializeField] List<GameObject> _paintings;
-
-    private void OnEnable()
-    {
-        foreach (var painting in _paintings)
-        {
-            if (!painting.TryGetComponent(out PaintingSnapping paint)) { return; }
-            
-            Array.Resize(ref _paintingCollection, _paintingCollection.Length + 1);
-            _paintingCollection[_paintingCollection.Length - 1] = paint.GetSnappableObject();
-        }
-    }
+    [SerializeField] SnappableObject[] _paintingCollection;
+   
     public override bool IsSolved()
     {
-        List<bool> bools = new List<bool>();
-
-        // checks all the paintings in the inspector, adds their state into a list
-        // if all of the paintings are in the correct position, the function returns true
-        foreach(var painting in _paintings)
-        {
-            if (!painting.TryGetComponent(out PaintingSnapping paint)) { return false; }
-
-            if (paint._isInCorrectPosition) { bools.Add(true); }
-            else { bools.Add(false); }
-        }
-
-        if (bools.TrueForAll(x => x))
-        {
-            Debug.Log("the puzzle has been solved!");
-            return true;
-        }
         return false;
     }
 }
