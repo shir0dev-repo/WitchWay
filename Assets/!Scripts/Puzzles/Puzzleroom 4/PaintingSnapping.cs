46029c32cef11c72587a529dca345409dbe018e8
StarlionGames <starliongames.dev@gmail.com>
2025-08-05 22:41:48 -0400

    removing a bit more
    - removed even more code
    - added snap points to the snappable object list
    - made paintings smaller
    
    TODO: figure out how to snap the paintings onto the snap points when dragging

--- Assets/!Scripts/Puzzles/Puzzleroom 4/PaintingSnapping.cs
+++ Assets/!Scripts/Puzzles/Puzzleroom 4/PaintingSnapping.cs
@@ -1,8 +1,8 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PaintingSnapping : MonoBehaviour
 {
-    [SerializeField] WZInteractable interact;
     [SerializeField] string hint; // for debugging purposes
 
     bool _canPickUp = false;
@@ -12,9 +12,8 @@ public class PaintingSnapping : MonoBehaviour
     {
         if (!_canPickUp) { return; }
 
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetMouseButtonDown(0))
         {
-            interact.Interacted();
             Debug.Log(hint);
         }
     }
